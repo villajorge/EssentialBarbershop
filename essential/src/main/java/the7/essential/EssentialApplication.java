af73bf30e11b6bef040e3eda9bf1687227ee47a5
unknown <villajorge41@gmail.com>
2022-11-29 16:16:24 -0600

    proj files

---
+++ essential/src/main/java/the7/essential/EssentialApplication.java
@@ -0,0 +1,13 @@
+package the7.essential;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EssentialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EssentialApplication.class, args);
+	}
+
+}
---
+++ essential/src/test/java/the7/essential/EssentialApplicationTests.java
@@ -0,0 +1,13 @@
+package the7.essential;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EssentialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
